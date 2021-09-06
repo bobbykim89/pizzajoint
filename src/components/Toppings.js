11c8e1c6d4db52ba9019ad598372d0ca664eb85b
bobbykim89 <bobby.sihun.kim@gmail.com>
2021-09-05 21:45:17 -0700

    some more progress with framer-motion

--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -2,6 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const containerVariants = {
+  hidden: {
+    x: '100vw',
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      delay: 0.5,
+    },
+  },
+  exit: {
+    x: '-100vw',
+    transition: { ease: 'easeInOut' },
+  },
+};
+
 const Toppings = ({ addTopping, pizza }) => {
   let toppings = [
     'mushrooms',
@@ -12,8 +31,26 @@ const Toppings = ({ addTopping, pizza }) => {
     'tomatoes',
   ];
 
+  const buttonVariants = {
+    hover: {
+      scale: 1.1,
+      textShadow: '0px 0px 8px rgb(255, 255, 255)',
+      boxShadow: '0px 0px  8px rgb(255, 255, 255',
+      transition: {
+        duration: 0.3,
+        yoyo: Infinity,
+      },
+    },
+  };
+
   return (
-    <div className='toppings container'>
+    <motion.div
+      className='toppings container'
+      variants={containerVariants}
+      initial='hidden'
+      animate='visible'
+      exit='exit'
+    >
       <h3>Step 2: Choose Toppings</h3>
       <ul>
         {toppings.map((topping) => {
@@ -32,17 +69,11 @@ const Toppings = ({ addTopping, pizza }) => {
       </ul>
 
       <Link to='/order'>
-        <motion.button
-          whileHover={{
-            scale: 1.1,
-            textShadow: '0px 0px 8px rgb(255, 255, 255)',
-            boxShadow: '0px 0px  8px rgb(255, 255, 255',
-          }}
-        >
+        <motion.button variants={buttonVariants} whileHover='hover'>
           Order
         </motion.button>
       </Link>
-    </div>
+    </motion.div>
   );
 };
 
